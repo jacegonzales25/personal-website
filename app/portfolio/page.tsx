20c5ff5ff23510f273531cf437368d8797a2f9eb
Jace Gonzales <jacegonzales25@gmail.com>
2023-08-07 16:34:22 +0800

    Progress 1

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,21 @@
+export default function App(){
+    return (
+        <div>
+            {/* Header layout */}
+
+            {/* Hero */}
+
+            {/* About */}
+
+            {/* Experience */}
+
+            {/* Skills */}
+
+            {/* Projects */}
+
+            {/* Contact Me */}
+            
+            <h1 className="text-red-500">Home</h1>
+        </div>
+    )
+}
