0be17a3d6064ce71c553e6d95a51e64ba53b4999
Jace Gonzales <jacegonzales25@gmail.com>
2023-08-15 20:27:41 +0800

    Start with new feature branch

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 export default function Header() {
     return (
-        <header className="flex justify-center">
-            <h1 className="scroll-m-20 text-xl font-semibold tracking-tight">Jace.Dev</h1>
+        <header className="flex mx-4 md:mx-40 items-start justify-between">
+            <div>
+                <h1 className="scroll-m-20 text-xl font-semibold tracking-tight">Jace.Dev</h1>
+            </div>
+
+            <div className="">
+                <h1 className="scroll-m-20 text-xl font-semibold tracking-tight">Jace.Dev</h1>
+                <h1 className="scroll-m-20 text-xl font-semibold tracking-tight">Jace.Dev</h1>
+                <h1 className="scroll-m-20 text-xl font-semibold tracking-tight">Jace.Dev</h1>
+            </div>
         </header>
     )
 }
